428c7fcb056b894af7ceb94328330b8075d57085
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-06-14 20:05:28 +0300

    Added validation constants

---
+++ AiCalendar.WebApi/Constants/EventConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace AiCalendar.WebApi.Constants
+{
+    public static class EventConstants
+    {
+        //Title
+        public const string TitleRequiredMessage = "Title is required.";
+        public const int TitleMinLength = 5;
+        public const int TitleMaxLength = 100;
+        public const string TitleLengthErrorMessage = "Title must between {1} and {2} characters long.";
+
+        //Description
+        public const int DescriptionMinLength = 30;
+        public const int DescriptionMaxLength = 2000;
+        public const string DescriptionLengthErrorMessage = "Description must between {1} and {2} characters long.";
+
+
+    }
+}
---
+++ AiCalendar.WebApi/Constants/UserConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace AiCalendar.WebApi.Constants
+{
+    public static class UserConstants
+    {
+        //UserName
+        public const string UserNameRequiredMessage = "User name is required.";
+        public const int UserNameMinLength = 3;
+        public const int UserNameMaxLength = 30;
+        public const string UserNameLengthErrorMessage = "User name must be between {1} and {2} characters long.";
+
+        //Email
+        public const string EmailRequiredMessage = "Email is required.";
+        public const string EmailFormatErrorMessage = "Email format is invalid.";
+
+        //Password 
+        public const string PasswordRequiredMessage = "Password is required.";
+    }
+}
