707a7c9a271c3b540e893edd2c73a6331b7c5186
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-06-17 13:12:12 +0300

    Added ClaimPrincipalExterntions static class

---
+++ AiCalendar.WebApi/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+
+namespace AiCalendar.WebApi.Extensions
+{
+    public static class ClaimPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal user)
+        {
+            string? userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+           
+            return userId;
+        }
+
+        public static string? GetUserName(this ClaimsPrincipal user)
+        {
+            string? userName = user.FindFirstValue("username");
+
+            return userName;
+        }
+
+        public static string? GetEmail(this ClaimsPrincipal user)
+        {
+            string? email = user.FindFirstValue(ClaimTypes.Email);
+            return email;
+        }
+    }
+}
