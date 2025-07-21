116355b48178cbb125fdf9480fc36c5106acbc01
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-21 18:13:02 +0300

    Initialize the class for EventParticipantsService tests

---
+++ AiCalendar.Tests/EventParticipantsServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AiCalendar.WebApi.Data.Repository;
+using AiCalendar.WebApi.Models;
+using AiCalendar.WebApi.Services.EventParticipants;
+using AiCalendar.WebApi.Services.EventParticipants.Interfaces;
+
+namespace AiCalendar.Tests
+{
+    public class EventParticipantsServiceTests : InMemoryDbTestBase
+    {
+        private IRepository<Participant> _participantRepository;
+        private IEventParticipantsService _eventParticipantsService;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            await Init();
+            _participantRepository = new Repository<Participant>(_context);
+            _eventParticipantsService = new EventParticipantsService(_participantRepository);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            // Clean up the in-memory database after each test
+            await Dispose();
+        }
+    }
+}
