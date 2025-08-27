dccff3429e9a3a89eefc889efad1eaa70704d325
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-08-27 16:31:14 +0300

    Added Login and Register pages

---
+++ AiCalendar.Blazor/ViewModels/Users/LoginAndRegisterInputViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AiCalendar.Blazor.Constants;
+
+namespace AiCalendar.Blazor.ViewModels.Users
+{
+    public class LoginAndRegisterInputViewModel
+    {
+        [Required(ErrorMessage = UserConstants.UserNameRequiredMessage)]
+        [StringLength(UserConstants.UserNameMaxLength, ErrorMessage = UserConstants.UserNameLengthErrorMessage, MinimumLength = UserConstants.UserNameMinLength)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = UserConstants.EmailRequiredMessage)]
+        [EmailAddress(ErrorMessage = UserConstants.EmailFormatErrorMessage)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = UserConstants.PasswordRequiredMessage)]
+        [StringLength(UserConstants.PasswordMaxLength, ErrorMessage = UserConstants.PasswordLengthErrorMessage, MinimumLength = UserConstants.PasswordMinLength)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
