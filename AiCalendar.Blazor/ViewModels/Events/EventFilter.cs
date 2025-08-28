01dacd577932826440512c8052d15da185a6620c
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-08-29 00:16:41 +0300

    Added events page and logic for fetching user events

---
+++ AiCalendar.Blazor/ViewModels/Events/EventFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace AiCalendar.Blazor.ViewModels.Events
+{
+    public class EventFilter
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool? IsCancelled { get; set; }
+    }
+}
